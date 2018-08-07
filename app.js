176cc538f67d1501ac6b988445b85e9651745dec
arisupriatna14 <arisupriatna703@gmail.com>
2018-08-07 16:48:22 +0700

    edit app and bin/www

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const logger = require('morgan');
 const mongoose = require('mongoose')
 
 //mongodb://localhost:27017/library
-mongoose.connect('mongodb://root:ari112233@ds215172.mlab.com:15172/library', { useNewUrlParser: true })
+mongoose.connect(process.env.MONGO_URI || 'mongodb://root:ari112233@ds215172.mlab.com:15172/library', { useNewUrlParser: true })
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error'))
 db.once('open', () => {
