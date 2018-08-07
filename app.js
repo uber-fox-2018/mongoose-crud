bbde4f8ba321836128fc4a72699c3512b0a910e4
arisupriatna14 <arisupriatna703@gmail.com>
2018-08-07 16:27:34 +0700

    edit app.js and add .env and Procfile

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
@@ -5,7 +6,8 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/library', { useNewUrlParser: true })
+//mongodb://localhost:27017/library
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error'))
 db.once('open', () => {
